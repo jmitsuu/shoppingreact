4c4d2a4d1defd2b7bf6d9799f754a273c6088875
Jairo Miguel <jmiguelafonso@hotmail.com>
2024-04-01 23:05:16 -0300

    component progress

---
+++ src/components/ProgressPayment.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { AiOutlineFileDone } from "react-icons/ai";
+import { MdOutlineFactCheck, MdPayment } from "react-icons/md";
+import { useLocation } from "react-router";
+import { Progress } from "./ui/progress";
+
+export function ProgressPayment() {
+  const path = useLocation();
+
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    if (path.pathname === "/check/payment") {
+      setProgress(100);
+      console.log(path);
+    } else {
+      setProgress(0);
+    }
+  }, [path]);
+  return (
+    <div className="flex items-center  mb-14  justify-center">
+      <div className="relative">
+        <h4 className="absolute top-10 text-slate-500 text-xs">Check</h4>
+        <MdOutlineFactCheck className="h-10 w-10 text-orange-500" />
+      </div>
+      <Progress value={progress} className="xl:w-44 w-20 " />
+      <div className="relative">
+        <h4 className="absolute top-10  text-slate-500 text-xs">Pagamento</h4>
+        <MdPayment
+          className={`h-10 w-10 ${progress === 100 && "text-orange-500"}`}
+        />
+      </div>
+      <Progress value={0} className="xl:w-44 w-20 " />
+      <div className="relative">
+        <h4 className="absolute top-10  text-slate-500 text-xs">Concluido</h4>
+        <AiOutlineFileDone className={`h-10 w-10`} />
+      </div>
+    </div>
+  );
+}
--- src/pages/Bag.tsx
+++ src/pages/Bag.tsx
@@ -1,21 +1,10 @@
+import { ProgressPayment } from "@/components/ProgressPayment";
 import { useCart } from "@/store/CartStore";
-import { Outlet, useLocation } from "react-router";
-import { MdOutlineFactCheck, MdPayment } from "react-icons/md";
-import { AiOutlineFileDone } from "react-icons/ai";
-import { Progress } from "@/components/ui/progress";
-import { useEffect, useState } from "react";
+import { Outlet } from "react-router";
 
 export function Bag() {
-  const path = useLocation();
   const { cart } = useCart();
-  const [progress, setProgress] = useState(0);
-  useEffect(() => {
-    if (path.pathname === "/check/payment") {
-      setProgress(100);
-    } else {
-      setProgress(0);
-    }
-  }, [path]);
+
   if (cart.length === 0) {
     return (
       <div className="text-center text-slate-700 font-bold text-4xl">
@@ -28,24 +17,7 @@ export function Bag() {
       <h1 className="text-slate-600 text-2xl font-bold mb-10">
         Informações de pagamento
       </h1>
-      <div className="flex items-center  mb-14  justify-center">
-        <div className="relative">
-          <h4 className="absolute top-10 text-slate-500 text-xs">Check</h4>
-          <MdOutlineFactCheck className="h-10 w-10 text-orange-500" />
-        </div>
-        <Progress value={progress} className="xl:w-44 w-20 " />
-        <div className="relative">
-          <h4 className="absolute top-10  text-slate-500 text-xs">Pagamento</h4>
-          <MdPayment
-            className={`h-10 w-10 ${progress === 100 && "text-orange-500"}`}
-          />
-        </div>
-        <Progress value={0} className="xl:w-44 w-20 " />
-        <div className="relative">
-          <h4 className="absolute top-10  text-slate-500 text-xs">Concluido</h4>
-          <AiOutlineFileDone className={`h-10 w-10`} />
-        </div>
-      </div>
+      <ProgressPayment />
       <div className=" xl:w-full xl:min-h-[600px]  flex  bg-white  ">
         <Outlet />
       </div>
