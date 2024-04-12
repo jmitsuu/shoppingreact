674dd791d5503c74a27a407454a9ee8e603299b1
jmitsuu <jmiguelafonso@hotmail.com>
2024-04-12 17:43:34 -0300

    react hook form

--- src/pages/auth/SingIn.tsx
+++ src/pages/auth/SingIn.tsx
@@ -2,9 +2,18 @@ import { ProfileForm } from "@/components/ProfileForm";
 import { Button } from "@/components/ui/button";
 import { PiLinkedinLogo } from "react-icons/pi";
 import { LiaInstagram } from "react-icons/lia";
-
+import { useForm } from "react-hook-form";
+interface typeInput{
+inputEmail?:string | "", 
+inputPassword?:string | ""
+}
 
 export function SingIn(){
+  const {register, handleSubmit, formState:{errors}} = useForm()
+  function pushUser({inputEmail, inputPassword}:typeInput){
+ console.log(inputEmail, inputPassword)
+  }
+ 
   return(
     <div className="w-full h-full flex mt-10 flex-col justify-center items-center">
       <h1 className="text-gray-500 text-5xl  border-b-2 pb-3">Bem vindo!</h1>
@@ -21,17 +30,29 @@ export function SingIn(){
       </a>
      </div>
      </div>
-    <form className=" xl:w-96 p-4">
-    <ProfileForm  formLabel="E-mail" formType="email" formPlace="digite seu e-mail"/>
-    <ProfileForm  formLabel="Senha" formType="password" formPlace="digite sua senha"/>
-    <Button className="w-full mt-5 h-12 bg-[#483584] hover:bg-[#5842a1]">Entrar</Button>
+    <form onSubmit={handleSubmit(pushUser)} className=" xl:w-96 p-4">
+    <ProfileForm  
+    formName={{...register("inputEmail",{required:"Preencha o e-mail"})} } 
+    formError={errors.inputEmail?.message} 
+    formLabel="E-mail" 
+    formType="email" 
+    formPlace="digite seu e-mail"/>
+   
+    <ProfileForm   
+    formName={{...register("inputPassword", {required:"Preencha a senha"})} } 
+    formError={errors.inputPassword?.message} 
+    formLabel="Senha" 
+    formType="password" 
+    formPlace="digite sua senha"/>
+    
+    <Button type="submit" className="w-full mt-5 h-12 bg-[#483584] hover:bg-[#5842a1]">Entrar</Button>
     <div className="text-xs text-center w-full mt-10">
     <span className="hover:text-gray-800 text-gray-600 cursor-pointer" >Esqueceu sua senha?</span>
     </div>
 
     </form>
       </div>
-    <div className="xl:min-w-[430px]  xl:min-h-full xl:rounded-r-md flex flex-col items-center justify-center bg-gradient-to-tr from-[#303164]
+    <div className="xl:min-w-[430px] p-4  xl:min-h-full xl:rounded-r-md flex flex-col items-center justify-center bg-gradient-to-tr from-[#303164]
     via-[#483584]
     to-[#303164]">
       <h1 className="text-gray-300 font-bold text-3xl">Bem vindo a Oriaj</h1>
