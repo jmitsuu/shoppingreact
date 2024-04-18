850336c114f5ff4bd7481539866927d333570334
jmitsuu <jmiguelafonso@hotmail.com>
2024-04-18 17:59:39 -0300

    dev

--- src/pages/admin/Management.tsx
+++ src/pages/admin/Management.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+
 import {
  Table,
  TableBody,
@@ -9,13 +9,23 @@ import {
 } from "@/components/ui/table";
 import { arrItems } from "@/interfaces/ProductInterface";
 import { FetchProducts } from "@/api/products/FetchProducts";
+import { FaTrash, FaPencilAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 export function Management() {
  const { products, isLoading } = FetchProducts();
+ const [page, setPage ] = useState<any>([])
  const limitedProducts = products.slice(0, 10);
+ console.log(products.length / 10)
+ useEffect(()=>{
+  for(let i = 1; i < products.length / 10; i ++){
+    setPage((state:any) => [...state, i])
+   }
+},[])
  if (isLoading) {
   return <div>...</div>;
  }
+
  return (
   <div className="w-screen h-full container  justify-center items-center">
    <h1 className="text-gray-500 text-2xl md:text-4xl mb-20 text-center">
@@ -25,8 +35,9 @@ export function Management() {
     {/* <TableCaption>Voce tem {cart.length} item(s) no carrinho.</TableCaption> */}
     <TableHeader>
      <TableRow>
-      <TableHead className="t">#</TableHead>
-      <TableHead className="text-center">Imagem</TableHead>
+      <TableHead className="text-center">#</TableHead>
+      <TableHead className="">Imagem</TableHead>
+      <TableHead className="text-center">Titulo</TableHead>
       <TableHead className="text-center">Preço</TableHead>
       <TableHead className="text-center">#</TableHead>
      </TableRow>
@@ -35,24 +46,22 @@ export function Management() {
      {limitedProducts.map((item: arrItems) => {
       return (
        <TableRow key={item._id} className="">
+         <TableCell className="font-bold text-center">{item._id}</TableCell>
         <TableCell className="font-bold text-center">
          <img src={item.image_url} className="h-10" />
         </TableCell>
         <TableCell className="font-bold text-center">{item.title}</TableCell>
         <TableCell className="font-bold text-center">{item.price}</TableCell>
-        <TableCell className="font-bold text-center ">
-         <Button className="bg-gray-300 text-bold hover:text-white ">
-          Editar
-         </Button>
-         <Button className="bg-red-300 text-bold ml-4 hover:text-white">
-          Deletar
-         </Button>
+        <TableCell className="font-bold text-center flex items-center justify-center gap-x-4 ">
+        <FaPencilAlt  className="text-blue-500 text-2xl"/>
+        <FaTrash  className="text-red-500 text-2xl"/>
         </TableCell>
        </TableRow>
       );
      })}
     </TableBody>
    </Table>
+   {page}
   </div>
  );
 }
